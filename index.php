573b474d490e4dacb117e9f0206041e134510647
wendellunny <wendelferreiraportugal@gmail.com>
2020-06-17 17:57:02 -0300

    tpl Site

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php 
 
 require_once("vendor/autoload.php");
-
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-	$results=$sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	$page = new Page();
+	$page -> setTpl("index");
+	
+});
+
+$app->get('/opa',function(){
+	require_once('pages/opa.html');
 });
 
 $app->run();
