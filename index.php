003723599180d5fd563f3d33ac990dc4f2ff313e
wendellunny <wendelferreiraportugal@gmail.com>
2020-06-18 00:48:34 -0300

    Criando o PageAdmin

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 $app = new Slim();
 
 $app->config('debug', true);
@@ -13,10 +14,12 @@ $app->get('/', function() {
 	
 });
 
-$app->get('/opa',function(){
-	require_once('pages/opa.html');
-});
+$app->get('/admin',function(){
+	$page = new PageAdmin();
+	$page->setTpl("index");
 
+
+});
 $app->run();
 
  ?>
