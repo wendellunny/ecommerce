5b3de44870c48b6157f62eecb510bf4dc1b0f1c5
wendellunny <wendelferreiraportugal@gmail.com>
2020-07-03 02:50:38 -0300

    Site-Categorias

--- index.php
+++ index.php
@@ -192,6 +192,16 @@
 
 	});
 
+	$app->get("/categories/:idcategory",function($idcategory){
+		$category = new Category();
+		$category->get((int)$idcategory);
+		$page = new Page();
+		$page -> setTpl("category",[
+			'category' => $category->getValues(),
+			'products' => []
+		]);
+	});
+
 
 
 
