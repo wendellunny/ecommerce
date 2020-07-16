cdbdd1fbe8ddc0ec830a1059775fec4390510add
wendellunny <wendelferreiraportugal@gmail.com>
2020-07-16 20:59:55 -0300

    Carrinho de Compras

--- site.php
+++ site.php
@@ -2,6 +2,7 @@
 	use \Hcode\Page;
 	use \Hcode\Model\Product;
 	use \Hcode\Model\Category;
+	use \Hcode\Model\Cart;
     
     $app->get('/', function() {
 		$products = Product::listAll();
@@ -45,8 +46,12 @@
 			"product" => $product->getValues(),
 			"categories" => $product->getCategories()
 		]);
-	})
+	});
 
-
+	$app->get("/cart",function(){
+		$cart = Cart::getFromSession();
+		$page = new Page();
+		$page->setTpl("cart");
+	});
 
 ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,6 +11,31 @@
         const SECRET1 = "HcodePhp7_Secret";
         const SECRET_IV = "WendelLuizSecret";
 
+        public static function getFromSession(){
+            $user = new User();
+            if(isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]["iduser"]>0){
+               
+                $user->setData($_SESSION[User::SESSION]);    
+            }
+            return $user;
+        }
+
+        public static function checkLogin($inadmin = true){
+            if(!isset($_SESSION[User::SESSION]) || !$_SESSION[User::SESSION] || !(int)$_SESSION[User::SESSION]["iduser"]>0){
+                //Não está logado
+                return false;
+            }else{
+                if($inadmin === true && (bool)$_SESSION[User::SESSION]["inadmin"] === true ){
+                    return true;
+                }else if($inadmin === false){
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+            
+        }
+
 
         public static function login ($login,$password){
             $sql = new Sql();
@@ -42,7 +67,7 @@
 
 
         public static function verifyLogin($inadmin = true){
-            if  (!isset($_SESSION[User::SESSION]) || !$_SESSION[User::SESSION] || !(int)$_SESSION[User::SESSION]["iduser"]>0 || (bool)$_SESSION[User::SESSION]["inadmin"]!==$inadmin){
+            if  (User::checkLogin($inadmin)){
                 header ("Location: /admin/login");
                 exit;
             }
